150086ee2cd3f04fc01e8b933f9bc4ac6e770668
Daniel Ajadi <ajadidan@gmail.com>
2019-10-18 21:24:11 -0400

    MVP complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./components/Header.js";
+import styled from "styled-components";
 import {
   BrowserRouter as Router,
   Switch,
@@ -9,6 +10,24 @@ import {
 import WelcomePage from "./components/WelcomePage";
 import CharacterList from "./components/CharacterList";
 
+const HeaderLink = styled.a`
+width: 100px;
+  height: 30px;
+  background: ${props => (props.primary ? "#FFF" : "#2a2223")};
+  color: ${props => (props.primary ? "#2a2223" : "#FFF")};
+  border: 0;
+  margin: 5px 10px;
+  transition: 0.2s ease-in;
+  border: ${props =>
+    props.primary ? "2px solid #99f3eb" : "2px solid #2a2223"};
+  &:hover {
+    background: ${props => (props.primary ? "#2a2223" : "#fff")};
+    color: ${props => (props.primary ? "#fff" : "#2a2223")};
+    border: ${props =>
+      props.primary ? "2px solid #2a2223" : "2px solid #99f3eb"};
+  }
+`;
+
 
 export default function App() {
   return (
@@ -16,8 +35,8 @@ export default function App() {
       <Header />
 
       <Router>
-        <Link to="/">Welcome Page</Link>
-        <Link to="/characterlist">Character List</Link>  
+        <HeaderLink> <Link to="/">Welcome Page</Link> </HeaderLink>
+        <HeaderLink> <Link to="/characterlist">Character List</Link> </HeaderLink>  
 
         <hr />
 
